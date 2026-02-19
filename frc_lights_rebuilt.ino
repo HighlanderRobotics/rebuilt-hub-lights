dad53fc9a7fd5862affa8113df312f9219f8bb5e
321gobstopper <321gobstopper@gmail.com>
2026-02-18 19:49:27 -0800

    Fix audio cutoff by adding delays after play commands
    Add 200ms delay after each myMP3.play() call to ensure DFPlayer has time to process the command and start playing audio before continuing execution. Prevents sounds from being cut off during state transitions.

--- frc_lights_rebuilt.ino
+++ frc_lights_rebuilt.ino
@@ -20,7 +20,7 @@
   #define NUM_LEDS_BLUE 30      // Can increase to 60 with Arduino Mega
 
   // ==================== DEBUG CONFIGURATION ====================
-  #define AUTOSTART false       // Set to true for auto-start with blue winning auto
+  #define AUTOSTART true       // Set to true for auto-start with blue winning auto
 
   // ==================== TIMING CONSTANTS (milliseconds) ====================
   #define AUTO_DURATION 20000
@@ -135,22 +135,27 @@
     switch(newState) {
       case AUTO:
         myMP3.play(AUDIO_START);  // Play start sound
+        delay(200);  // Give DFPlayer time to start playing
         break;
 
       case AUTO_PAUSE:
         myMP3.play(AUDIO_END);  // Play end sound (end of auto)
+        delay(200);  // Give DFPlayer time to start playing
         break;
 
       case TRANSITION:
         myMP3.play(AUDIO_RESUME);  // Play resume sound (teleop begins)
+        delay(200);  // Give DFPlayer time to start playing
         break;
 
       case ENDGAME:
         myMP3.play(AUDIO_WARNING);  // Play warning sound
+        delay(200);  // Give DFPlayer time to start playing
         break;
 
       case MATCH_OVER:
         myMP3.play(AUDIO_END);  // Play end sound (match complete)
+        delay(200);  // Give DFPlayer time to start playing
         break;
     }
 
